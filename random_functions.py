6e4268fb72a74dfa84f8862c37db560a15cc0c2b
mistBourne1300 <gcford99@gmail.com>
2021-11-04 10:50:41 -0600

    added more random functions and worked on TTTmachine

--- random_functions.py
+++ random_functions.py
@@ -33,24 +33,42 @@ def random_point_in_unit_circle_polar_method():
 	r = np.sqrt(random.random())
 	return r*np.cos(theta), r*np.sin(theta)
 
+def two_sample_degrees_of_freedom(varx, numx, vary, numy): # does not return the right value
+	_x = varx/numx
+	_y = vary/numy
+	num = (_x + _y)**2
+	denom = (_x**2)/(numx-1) + (_y**2)/(numy-1)
+	return num / denom
+
+def choose_5_random():
+	return np.sort([np.random.uniform() for i in range(5)])
+
+
 if __name__ == "__main__":
-	from matplotlib import pyplot as plt
-	# print(estimate_pi(10000))
-	points = []
-	for i in range(1000):
-		points.append(random_point_in_unit_circle_cartesian_method())
-		plt.scatter(points[-1][0], points[-1][1], s=1)
-	print(points)
-	plt.show()
+	# from matplotlib import pyplot as plt
+	# # print(estimate_pi(10000))
+	# points = []
+	# for i in range(1000):
+	# 	points.append(random_point_in_unit_circle_cartesian_method())
+	# 	plt.scatter(points[-1][0], points[-1][1], s=1)
+	# #print(points)
+	# #plt.show()
 
-	points = []
-	for i in range(1000):
-		points.append(non_random_point_in_unit_circle_polar_method())
-		plt.scatter(points[-1][0], points[-1][1], s=1)
-	plt.show()
+	# points = []
+	# for i in range(1000):
+	# 	points.append(non_random_point_in_unit_circle_polar_method())
+	# 	plt.scatter(points[-1][0], points[-1][1], s=1)
+	# #plt.show()
 
-	points = []
-	for i in range(1000):
-		points.append(random_point_in_unit_circle_polar_method())
-		plt.scatter(points[-1][0], points[-1][1], s=1)
-	plt.show()
+	# points = []
+	# for i in range(1000):
+	# 	points.append(random_point_in_unit_circle_polar_method())
+	# 	plt.scatter(points[-1][0], points[-1][1], s=1)
+	# plt.show()
+
+	print(two_sample_degrees_of_freedom(234.6964, 8, 183.0714, 8))
+
+	# less_than_third = []
+	# for i in range(100000):
+	# 	less_than_third.append(choose_5_random()[2] < 1/3)
+	# print(f'probability: {sum(less_than_third) / len(less_than_third)}')
--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 import numpy as np
+from imageio import imread
+from imageio import imwrite
+from matplotlib import pyplot as plt
 
-zeroMat = np.zeros((10,10))
+# image = imread("/Users/chase/Downloads/IMG_0124.JPG")
+# scaled = image / 255
+# plt.imshow(scaled)
+# plt.show()
+# greyscale = scaled.mean(axis=2)
+# plt.imshow(greyscale, cmap = "magma")
+# plt.show()
 
+states = np.array(["hot", 'mild', 'cold', 'freezing'])
+choice = np.array([0,0,1,0])
+mask = choice == 1
+print(states[mask]) # prints a list containing the label chosen
+print(states[np.argmax(choice)]) # they want just the label, not the array containing the label
+
+A = np.array([[1,2],[1,2]])
+print(np.sum(A, axis = 0))
