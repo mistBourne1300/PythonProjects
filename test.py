d245e244f937ef75ecd88f52ec9b9f47c3e7ad75
mistBourne1300 <gcford99@gmail.com>
2022-03-02 22:44:05 -0700

    update as of 2022-03-02

--- test.py
+++ test.py
@@ -6,17 +6,31 @@ import numpy as np
 import time
 # import panphon
 
-# dst = panphon.distance.Distance()
-N = 2000
-print_times = []
-print()
-for i in range(N+1):
-	per = int(100*i/N)
-	start = time.time()
-	print(end = " [")
-	[print(end = "#") for i in range(per)]
-	[print(end = " ") for i in range(100-per)]
-	print(f'] {i}/{N} ({per}%)', end = "\r")
-	print_times.append(time.time() - start)
-	time.sleep(5/N)
-print(f'\naverage time to print progress bar: {np.mean(print_times)}\n')
+
+# N = 2000
+# print_times = []
+# print()
+# for i in range(N+1):
+# 	per = int(100*i/N)
+# 	start = time.time()
+# 	print(end = " [")
+# 	[print(end = "#") for i in range(per)]
+# 	[print(end = " ") for i in range(100-per)]
+# 	print(f'] {i}/{N} ({per}%)', end = "\r")
+# 	print_times.append(time.time() - start)
+# 	time.sleep(5/N)
+# print(f'\naverage time to print progress bar: {np.mean(print_times)}\n')
+
+import gym
+env = gym.make("Acrobot-v1")
+
+try:
+	env.reset()
+	done = False
+	while not done:
+		env.render()
+		obs, reward, done, info = env.step(env.action_space.sample())
+		if done:
+			break
+finally:
+	env.close()
